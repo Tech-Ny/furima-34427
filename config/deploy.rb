bc0e628051e18903e8124f7123c0e840a25e137d
Tech-Ny <mbb.youkey@gmail.com>
2021-02-17 14:47:06 +0900

    試行錯誤

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,13 @@ set :repo_url,  'git@github.com:Tech-Ny/furima-34427.git'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],keys: ['~/.ssh/sample.pem'] 
+set :ssh_options, auth_methods: ['authorized_keys'],
+                  keys: ['~/.ssh/sample.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
