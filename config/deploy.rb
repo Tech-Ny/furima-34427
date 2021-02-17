72851f5432a86cfd9538d168fba2a50545ef4fd7
Tech-Ny <mbb.youkey@gmail.com>
2021-02-17 14:48:06 +0900

    key変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,8 +15,8 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['authorized_keys'],
-                  keys: ['~/.ssh/sample.pem'] 
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/authorized_keys'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
