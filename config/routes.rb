5c2b544f5ebf7815850b40da1c0061938c368b11
Tech-Ny <mbb.youkey@gmail.com>
2021-02-19 15:49:02 +0900

    user routing変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'cards/new'
-  get 'users/show'
-  devise_for :users
+  #get 'cards/new'
+  #get 'users/show'
   root to: "items#index"
   resources :items do
     resources :orders, only: [:index, :new, :create]
   end
 
   resources :users, only: [:show, :update]
+  devise_for :users
   resources :cards, only: [:new, :create]
 
 end
